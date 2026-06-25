c09a7b34378f1c35c87376e1d669417eaa18bc6b
Vishal05it <vishal.tiwari0510@gmail.com>
2026-06-25 21:10:24 +0530

    Firstly parse cookies

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 dotenv.config();
 const app = express();
-app.use(express.json());
 app.use(cookieParser());
+app.use(express.json());
 export default app;
