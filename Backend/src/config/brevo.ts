37cb0bfeb399f0915ce3a29e2588a00deba27968
Vishal05it <vishal.tiwari0510@gmail.com>
2026-06-25 20:36:26 +0530

    BREVO KEY

--- Backend/src/config/brevo.ts
+++ Backend/src/config/brevo.ts
@@ -1,7 +1,7 @@
 import { BrevoClient } from "@getbrevo/brevo";
-if (!process.env.BREVO_PASSWORD) {
+if (!process.env.BREVO_KEY) {
   throw new Error("BREVO KEY MISSING");
 }
 export const brevo = new BrevoClient({
-  apiKey: process.env.BREVO_PASSWORD!,
+  apiKey: process.env.BREVO_KEY!,
 });
