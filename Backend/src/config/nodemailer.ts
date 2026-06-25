99bca3f6693663486b1320c2527413fce50a4389
Vishal05it <vishal.tiwari0510@gmail.com>
2026-06-25 19:22:29 +0530

    BREVO USED

--- Backend/src/config/nodemailer.ts
+++ Backend/src/config/nodemailer.ts
@@ -1,24 +1,10 @@
 import nodemailer from "nodemailer";
 export const transport = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
   auth: {
-    user: process.env.EMAIL,
-    pass: process.env.APP_PASSWORD,
+    user: process.env.BREVO_LOGIN,
+    pass: process.env.BREVO_PASSWORD,
   },
 });
-transport.verify((err, success) => {
-  if (err) {
-    console.error("SMTP Verify Error:", err);
-  } else {
-    console.log("SMTP Ready");
-  }
-});
-console.log("EMAIL:", process.env.EMAIL);
-console.log(
-  "APP_PASSWORD exists:",
-  !!process.env.APP_PASSWORD,
-  "length:",
-  process.env.APP_PASSWORD?.length,
-);
