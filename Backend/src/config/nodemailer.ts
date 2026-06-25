36156aa4323d8a17f0c01810a05b8edbeb08afa3
Vishal05it <vishal.tiwari0510@gmail.com>
2026-06-25 19:06:54 +0530

    Nodemailer check

--- Backend/src/config/nodemailer.ts
+++ Backend/src/config/nodemailer.ts
@@ -8,3 +8,17 @@ export const transport = nodemailer.createTransport({
     pass: process.env.APP_PASSWORD,
   },
 });
+transport.verify((err, success) => {
+  if (err) {
+    console.error("SMTP Verify Error:", err);
+  } else {
+    console.log("SMTP Ready");
+  }
+});
+console.log("EMAIL:", process.env.EMAIL);
+console.log(
+  "APP_PASSWORD exists:",
+  !!process.env.APP_PASSWORD,
+  "length:",
+  process.env.APP_PASSWORD?.length,
+);
